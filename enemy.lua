08ad79dac047e726226fd4dcc3beefe8d44a2a4d
Aeshus <87614336+Aeshus@users.noreply.github.com>
2024-01-30 21:11:02 -0600

    refactor: correctly use lua metatables for modules
    Basically instead of just returning some weird table thing in the
    constructor, we're now properly using metatables to actually interface
    with lua properly.
    
    I'm assuming issues would've cropped up once more than just a single
    enemy would be on screen.
    
    I also took the liberty of running StyLua on it to lean up some of it.

--- enemy.lua
+++ enemy.lua
@@ -1,19 +1,33 @@
-enemy = {}
+local enemy = {}
+enemy.__index = enemy
 
-function enemy:new(x, y)
-   self.x = x
-   self.y = y
-   self.image = love.graphics.newImage("resources/enemy.jpg")
-   self.scale = 0.1
-   self.speed = 200
-   return enemy
+function enemy.new(x, y)
+	local e = setmetatable({}, enemy)
+
+	e.x = x
+	e.y = y
+	e.image = love.graphics.newImage("resources/enemy.jpg")
+	e.scale = 0.1
+	e.speed = 200
+
+	return e
 end
 
-function math.normalize(x,y) local l=(x*x+y*y)^.5 if l==0 then return 0,0,0 else return x/l,y/l,l end end
+function math.normalize(x, y)
+	local l = (x * x + y * y) ^ 0.5
+
+	if l == 0 then
+		return 0, 0, 0
+	else
+		return x / l, y / l, l
+	end
+end
 
 function enemy:update(dt)
-   -- handle movement and direction
-   normx, normy = math.normalize((self.x-player.x), (self.y-player.y))
-   self.x = self.x - dt * normx * enemy.speed
-   self.y = self.y - dt * normy * enemy.speed
+	normx, normy = math.normalize((self.x - player.x), (self.y - player.y))
+
+	self.x = self.x - dt * normx * self.speed
+	self.y = self.y - dt * normy * self.speed
 end
+
+return enemy
--- main.lua
+++ main.lua
@@ -1,27 +1,27 @@
-require("player")
-require("enemy")
+local playerFactory = require("player")
+local enemyFactory = require("enemy")
 
 function love.load()
-   player = player:new(300, 400) -- overriding the require...caution?
-   horde = enemy:new(0, 0)
-   
-   love.graphics.setNewFont(12)
-   love.graphics.setColor(0,0,0)
-   love.graphics.setBackgroundColor(255,255,255)
-   love.window.setMode(400, 400)
-   timeInit = love.timer.getTime()
+	player = playerFactory.new(300, 400)
+	horde = enemyFactory.new(0, 0)
+
+	love.graphics.setNewFont(12)
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.setBackgroundColor(255, 255, 255)
+	love.window.setMode(700, 700)
+	timeInit = love.timer.getTime()
 end
 
 function love.update(dt)
-   player:update(dt)
-   horde:update(dt)
+	player:update(dt)
+	horde:update(dt)
 end
 
 function love.draw()
-   --score system. Initial Time is subtracted from the current time to get the elapsed time and generate a score
-   timeCurr = love.timer.getTime() - timeInit
-   score = timeCurr * 20
-   love.graphics.draw(player.image, player.x, player.y, 0, player.scale, player.scale)
-   love.graphics.draw(horde.image, horde.x, horde.y, 0, horde.scale, horde.scale)
-   love.graphics.print("score: " .. math.ceil(score), 10, 10)
+	--score system. Initial Time is subtracted from the current time to get the elapsed time and generate a score
+	timeCurr = love.timer.getTime() - timeInit
+	score = timeCurr * 20
+	love.graphics.draw(player.image, player.x, player.y, 0, player.scale, player.scale)
+	love.graphics.draw(horde.image, horde.x, horde.y, 0, horde.scale, horde.scale)
+	love.graphics.print("score: " .. math.ceil(score), 10, 10)
 end

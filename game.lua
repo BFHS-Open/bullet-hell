de5e100d940a9ab2f61189a2addce26059694d0f
Joe Strauss <cuppajava@tutanota.com>
2024-02-26 14:58:15 -0600

    Add start screen

---
+++ game.lua
@@ -0,0 +1,79 @@
+local playerFactory = require("player")
+local enemyFactory = require("enemy")
+local Point2d = require("point2d")
+
+local game = {}
+game.__index = game
+
+function game.load()
+	Player = playerFactory.new(Point2d:rect(350, 350))
+	EnemyTable = {}
+
+	for i = 1, 6 do
+		EnemyTable[i] = CreateRandomEnemy(Player)
+	end
+end
+
+function CreateRandomEnemy(target)
+		local screenX, screenY = love.window.getMode()
+
+		local data = { target = target }
+
+		-- flip coin for axis
+		if love.math.random(0, 1) then
+			-- x axis, flip for left or right side
+
+			data.position = Point2d:rect(
+				love.math.random(0, 1) * screenX,
+				love.math.randomNormal(1/4, 1/2) * screenY
+			)
+		else
+			-- y axis, flip for top or bottom
+
+			data.position = Point2d:rect(
+				love.math.randomNormal(1/4, 1/2) * screenX,
+				love.math.random(0, 1) * screenY
+			)
+		end
+
+		-- generate type
+		local type = ({
+			"homing",
+			"ramming"
+		})[love.math.random(2)]
+
+		if type == "ramming" then
+			data.angle = love.math.random() * 360
+		end
+
+		return enemyFactory.new(type, data)
+end
+
+function game.update(dt)
+
+	-- update all enemies
+	for i, enemy in ipairs(EnemyTable) do
+		enemy:update(dt)
+
+		-- delete if dead
+		if not enemy.alive then
+			-- fast delete by moving the last element to i
+			local other = table.remove(EnemyTable)
+			if i <= #table then
+				table[i] = other
+			end
+		end
+	end
+
+	Player:update(dt)
+end
+
+function game.draw()
+	Player:draw()
+
+	for _,v in ipairs(EnemyTable) do
+		v:draw()
+	end
+end
+
+return game
--- main.lua
+++ main.lua
@@ -1,78 +1,36 @@
 local playerFactory = require("player")
 local enemyFactory = require("enemy")
 local Point2d = require("point2d")
+local game = require("game")
 
 function love.load()
-	love.graphics.setNewFont(12)
+	BigFont = love.graphics.newFont("resources/FiraCode-Regular.ttf", 36)
+	RegularFont = love.graphics.newFont("resources/FiraCode-Regular.ttf", 24)
 	love.graphics.setBackgroundColor(255, 255, 255)
+	BackgroundImage = love.graphics.newImage("resources/background.png")
 	love.window.setMode(700, 700)
-
-	Player = playerFactory.new(Point2d:rect(350, 350))
-	EnemyTable = {}
-
-	for i = 1, 6 do
-		EnemyTable[i] = CreateRandomEnemy(Player)
-	end
-end
-
-function CreateRandomEnemy(target)
-		local screenX, screenY = love.window.getMode()
-
-		local data = { target = target }
-
-		-- flip coin for axis
-		if love.math.random(0, 1) then
-			-- x axis, flip for left or right side
-
-			data.position = Point2d:rect(
-				love.math.random(0, 1) * screenX,
-				love.math.randomNormal(1/4, 1/2) * screenY
-			)
-		else
-			-- y axis, flip for top or bottom
-
-			data.position = Point2d:rect(
-				love.math.randomNormal(1/4, 1/2) * screenX,
-				love.math.random(0, 1) * screenY
-			)
-		end
-
-		-- generate type
-		local type = ({
-			"homing",
-			"ramming"
-		})[love.math.random(2)]
-
-		if type == "ramming" then
-			data.angle = love.math.random() * 360
-		end
-
-		return enemyFactory.new(type, data)
+	GameStarted = false
 end
 
 function love.update(dt)
-
-	-- update all enemies
-	for i, enemy in ipairs(EnemyTable) do
-		enemy:update(dt)
-
-		-- delete if dead
-		if not enemy.alive then
-			-- fast delete by moving the last element to i
-			local other = table.remove(EnemyTable)
-			if i <= #table then
-				table[i] = other
-			end
+	if not GameStarted then
+		if love.keyboard.isDown("space") then
+			GameStarted = true
+			game.load()
 		end
+	else
+		game.update(dt)
 	end
-
-	Player:update(dt)
 end
 
 function love.draw()
-	Player:draw()
+	love.graphics.draw(BackgroundImage)
+	if not GameStarted then
+		local screenX, screenY = love.window.getMode()
+		love.graphics.print("Bullet Hell", BigFont, screenX/2-100, screenY/2-120)
+		love.graphics.print("Press Space to Start", RegularFont, screenX/2-130, screenY/2+120)
 
-	for _,v in ipairs(EnemyTable) do
-		v:draw()
+	else
+		game.draw()
 	end
 end
